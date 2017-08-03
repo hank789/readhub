fa2adace2d17446bbec1e69051f9b5bab82ff17b
hank <hank.huiwang@gmail.com>
2017-08-03 19:08:40 +0800

    webview返回

--- resources/assets/js/mixins/Webview.js
+++ resources/assets/js/mixins/Webview.js
@@ -12,17 +12,33 @@ export default {
             var isPlusReady = navigator.userAgent.match(/Html5Plus/i); //TODO 5\+Browser?
 
     		if (isPlusReady){
-                var embed=plus.webview.create(url,'embed',{popGesture: 'hide',
+                function webviewBackButton(){
+                    var ws = plus.webview.getWebviewById(url);
+                    if (ws) {
+                        ws.close();
+                    }
+                }
+                console.log(plus.webview.currentWebview().id);
+
+                var embed=plus.webview.create(url, url,{popGesture: 'hide',
                     top:'0px',
                     bottom:'0px',
                     position:'dock',
                     dock:'bottom',
+                    backButtonAutoControl: 'hide',
                     titleNView: {
                         backgroundColor: '#f7f7f7', //导航栏背景色
                         titleText: title, //导航栏标题
                         titleColor: '#000000', //文字颜色
                         type: 'transparent', //透明渐变样式
-                        autoBackButton: true, //自动绘制返回箭头
+                        autoBackButton: false, //自动绘制返回箭头
+                        buttons: [{
+                            color: '#5e5e5e',
+                            'float': 'left',
+                            fontSize: '27px',
+                            text: '\u1438',
+                            onclick: webviewBackButton
+                        }],
                         splitLine: { //底部分割线
                             color: '#cccccc'
                         }
