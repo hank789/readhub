125aea57d618846fd8e2014ec05ff2010246471b
edwin <edwincoder@163.com>
2017-09-06 15:27:03 +0800

    列表打开方式修改

--- resources/views/h5-header.blade.php
+++ resources/views/h5-header.blade.php
@@ -1,4 +1,4 @@
-<header class="header-inwehub" :path="$route.path">
+<header class="header-inwehub" :path="$route.path" v-if="$route.path !== '/share'">
     <vui-back-button v-if="$route.path !== '/h5'"></vui-back-button>
     <div class="title" v-text="title"></div>
     <a class="shareBtn" @click="share()" v-if="/^\/c\//.test($route.path)">
